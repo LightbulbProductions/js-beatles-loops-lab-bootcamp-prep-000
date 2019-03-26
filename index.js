640e25f397e9c2ffce38fd90f158cb316b64e565
Michelle G <gerbermichelle23@gmail.com>
2019-03-26 14:20:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // add solution here
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-    
-  var johnLennonFacts, function(){
-    
-    }
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) 
+  {
+    var string = `${musicians[i]} plays ${instruments[i]}`;
+    array.push(string);
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var array = [];
+  let i = 0;
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+  return array;
+}
